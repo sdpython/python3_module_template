692a0897536d982351ae18e13bd2811d577d0f12
xavier <xavier.dupre@gmail.com>
2013-06-23 22:23:29 +0200

    changing the documentation theme

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -94,7 +94,8 @@ modindex_common_prefix = [
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+#html_theme = 'default'
+html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
