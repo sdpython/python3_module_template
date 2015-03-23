bc4c724bdacb0f28e166c11b878cb36e165d7503
dupre <xavier.dupre@ensae.fr>
2015-03-23 12:01:06 +0100

    update conf.py, remove unnecessary file

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -17,12 +17,6 @@ sys.path.insert(
     os.path.abspath(
         os.path.join(
             os.path.split(__file__)[0],
-            "project_name")))
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.split(__file__)[0],
             "..",
             "..",
             "..",
