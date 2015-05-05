41741f4a859b81665691c3d0d01c3717476ea189
dupre <xavier.dupre@ensae.fr>
2015-05-06 01:21:22 +0200

    update setup and add examples

---
+++ _unittests/ut_example/test_LONG_not_included.py
@@ -0,0 +1,62 @@
+"""
+@brief      test log(time=101s)
+
+You should indicate a time in seconds. The program ``run_unittests.py``
+will sort all test files by increasing time and run them.
+"""
+
+
+import sys
+import os
+import unittest
+
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+try:
+    import pyquickhelper
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    if "PYQUICKHELPER" in os.environ and len(os.environ["PYQUICKHELPER"]) > 0:
+        sys.path.append(os.environ["PYQUICKHELPER"])
+    import pyquickhelper
+
+
+from pyquickhelper import fLOG
+from src.project_name.subproject.myexample import myclass
+from src.project_name.subproject.myexampleb import myclassb
+from src.project_name.subproject2.myexample2 import myclass2
+
+
+class TestLONGExample (unittest.TestCase):
+
+    def test_long_void(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+        assert True
+
+if __name__ == "__main__":
+    unittest.main()
