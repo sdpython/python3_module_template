1008a095e75bf68c0bdba961b8f593f3a41f2fee
dupre <xavier.dupre@ensae.fr>
2015-05-06 00:55:37 +0200

    update setup

--- setup.py
+++ setup.py
@@ -84,20 +84,31 @@ package_data = {project_var_name + ".subproject": ["*.tohelp"]}
 
 
 def is_local():
-    if "clean_space" in sys.argv or \
-            "write_version" in sys.argv or \
-            "clean_pyd" in sys.argv or \
-            "build_sphinx" in sys.argv or \
-            "unittests" in sys.argv or \
-            "copy27" in sys.argv or \
-            "sdist" in sys.argv or \
-            "register" in sys.argv or \
-            "bdist_wininst" in sys.argv or \
-            "bdist_msi" in sys.argv or \
-            "bdist_wheel" in sys.argv or \
-            "build_script" in sys.argv or \
-            "copy_dist" in sys.argv or \
-            "upload_docs" in sys.argv:
+    if \
+       "bdist_msi" in sys.argv or \
+       "build27" in sys.argv or \
+       "build_script" in sys.argv or \
+       "build_sphinx" in sys.argv or \
+       "bdist_wheel" in sys.argv or \
+       "bdist_wininst" in sys.argv or \
+       "clean_pyd" in sys.argv or \
+       "clean_space" in sys.argv or \
+       "copy27" in sys.argv or \
+       "copy_dist" in sys.argv or \
+       "local_pypi" in sys.argv or \
+       "notebook" in sys.argv or \
+       "publish" in sys.argv or \
+       "publish_doc" in sys.argv or \
+       "register" in sys.argv or \
+       "unittests" in sys.argv or \
+       "unittests_LONG" in sys.argv or \
+       "unittests_SKIP" in sys.argv or \
+       "run27" in sys.argv or \
+       "sdist" in sys.argv or \
+       "setupdep" in sys.argv or \
+       "test_local_pypi" in sys.argv or \
+       "upload_docs" in sys.argv or \
+       "write_version" in sys.argv:
         return True
     else:
         return False
