97be6938c9cca5429ac4b2c03791bee38d26caf3
dupre <xavier.dupre@ensae.fr>
2015-05-14 19:55:24 +0200

    fix the setup to include extensions tohelp

--- setup.py
+++ setup.py
@@ -187,7 +187,8 @@ if is_local():
     pyquickhelper = import_pyquickhelper()
     r = pyquickhelper.process_standard_options_for_setup(
         sys.argv, __file__, project_var_name,
-        unittest_modules=["pyquickhelper"])
+        unittest_modules=["pyquickhelper"],
+        extra_ext=["tohelp"])
 else:
     r = False
 
