7abf0ac26212203e5ec822553fb9680b1cb786ea
dupre <xavier.dupre@ensae.fr>
2015-10-31 21:02:52 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -164,7 +164,9 @@ if is_local():
         extra_ext=["tohelp"],
         add_htmlhelp=sys.platform.startswith("win"),
         coverage_options=dict(omit=["*exclude*.py"]))
-    if not r:
+    if not r and not ({"bdist_msi", "sdist",
+                       "bdist_wheel", "publish", "publish_doc", "register",
+                       "upload_docs", "bdist_wininst"} & set(sys.argv)):
         raise Exception("unable to interpret command line: " + str(sys.argv))
 else:
     r = False
