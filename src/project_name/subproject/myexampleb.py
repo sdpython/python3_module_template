c56aace7978d6ad771a5801219bbc1a2b03103ae
dupre <xavier.dupre@ensae.fr>
2016-02-14 13:37:25 +0100

    pep8

--- src/project_name/subproject/myexampleb.py
+++ src/project_name/subproject/myexampleb.py
@@ -12,9 +12,9 @@ class myclassb(myclass):
     This is the documentation for this class.
 
     Inherits from @see cl myclass.
-    
+
     An example on how to share: :sharenet:`facebook-linkedin-twitter-20-body`.
-    
+
     """
 
     def __init__(self, pa):
