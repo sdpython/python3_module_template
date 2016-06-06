8aab6373648cd2db13e1d70fd9bca4e99b5b845a
dupre <xavier.dupre@ensae.fr>
2016-06-06 23:07:28 +0200

    documentation

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -18,12 +18,8 @@ sys.path.insert(
             "src")))
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables
-set_sphinx_variables(__file__,
-                     "project_name",
-                     "author(s)",
-                     2015,
-                     "wild",
-                     wild_sphinx_theme.get_theme_dir(),
-                     locals())
+set_sphinx_variables(__file__, "project_name", "author(s)", 2016,
+                     "wild", wild_sphinx_theme.get_theme_dir(), locals(),
+                     extlinks=dict(issue=('https://github.com/sdpython/python3_module_template/issues/%d', 'issue')))
 
 blog_root = "http://www.xavierdupre.fr/app/python3_module_template/helpsphinx/"
