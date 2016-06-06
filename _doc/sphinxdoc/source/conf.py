90ab9bcb0263f610659d0e9a50e92f6cb48a89fb
dupre <xavier.dupre@ensae.fr>
2016-06-06 23:55:21 +0200

    fix link to issues

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -20,6 +20,6 @@ sys.path.insert(
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 set_sphinx_variables(__file__, "project_name", "author(s)", 2016,
                      "wild", wild_sphinx_theme.get_theme_dir(), locals(),
-                     extlinks=dict(issue=('https://github.com/sdpython/python3_module_template/issues/%d', 'issue')))
+                     extlinks=dict(issue=('https://github.com/sdpython/python3_module_template/issues/%s', 'issue')))
 
 blog_root = "http://www.xavierdupre.fr/app/python3_module_template/helpsphinx/"
