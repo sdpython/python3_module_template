c756348c67f8a8e4819fba95e953264ccb6d6235
dupre <xavier.dupre@ensae.fr>
2016-08-27 19:18:12 +0200

    simplify conf_base

--- _doc/sphinxdoc/source/conf_base.py
+++ _doc/sphinxdoc/source/conf_base.py
@@ -1,10 +1,12 @@
-import sys
-import os
-import datetime
-import re
 import wild_sphinx_theme
 
-from pyquickhelper.helpgen.default_conf import set_sphinx_variables
+try:
+    from pyquickhelper.helpgen.default_conf import set_sphinx_variables
+except ImportError as e:
+    import os
+    import sys
+    paths = "\n".join(sys.path)
+    raise ImportError("pyquickhelper is not installed. PYTHONPATH={0}\nsys.path=\n{1}".format(os.environ.get("PYTHONPATH", None), paths))
 set_sphinx_variables(__file__, "project_name", "author(s)", 2016,
                      "wild", wild_sphinx_theme.get_theme_dir(), locals(),
                      extlinks=dict(issue=('https://github.com/sdpython/python3_module_template/issues/%s', 'issue')))
