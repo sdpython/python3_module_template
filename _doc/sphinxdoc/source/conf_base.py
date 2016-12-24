1e3b23899581ca5edb9d064993f64b6247f98023
xavier dupré <xavier.dupre@gmail.com>
2016-12-24 12:57:42 +0100

    documentation

--- _doc/sphinxdoc/source/conf_base.py
+++ _doc/sphinxdoc/source/conf_base.py
@@ -1,18 +1,23 @@
 import wild_sphinx_theme
 
 try:
-    from pyquickhelper.helpgen.default_conf import set_sphinx_variables
+    from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
 except ImportError as e:
     import os
     import sys
     paths = "\n".join(sys.path)
     raise ImportError("pyquickhelper is not installed. PYTHONPATH={0}\nsys.path=\n{1}".format(
         os.environ.get("PYTHONPATH", None), paths))
+
 set_sphinx_variables(__file__, "project_name", "author(s)", 2016,
-                     "wild", wild_sphinx_theme.get_theme_dir(), locals(),
+                     "wild", wild_sphinx_theme.get_theme_dir(), locals(), book=True,
                      extlinks=dict(issue=('https://github.com/sdpython/python3_module_template/issues/%s', 'issue')))
 
 blog_root = "http://www.xavierdupre.fr/app/python3_module_template/helpsphinx/"
+
+html_context = {
+    'css_files': get_default_stylesheet(),
+}
 
 
 def custom_latex_processing(latex):
@@ -25,4 +30,4 @@ def custom_latex_processing(latex):
     if latex is None:
         raise ValueError("Latex is null")
     # this weird modification is only needed when jenkins run a unit test in pyquickhelper (pycode)
-    return latex.replace("gynotebooks\\Python_logo_and_wordmark", "Python_logo_and_wordmark")
+    return latex
--- _doc/sphinxdoc/source/index.rst
+++ _doc/sphinxdoc/source/index.rst
@@ -1,4 +1,5 @@
 
+
 PROJECT_NAME documentation
 ==========================
 
