2a8752a75bc8c53c7d184934410532cc1f5f395b
xavier dupré <xavier.dupre@gmail.com>
2017-08-11 00:56:36 +0200

    change the default template to readthedocs

--- _doc/sphinxdoc/source/conf_base.py
+++ _doc/sphinxdoc/source/conf_base.py
@@ -1,4 +1,4 @@
-import wild_sphinx_theme
+import sphinx_rtd_theme
 
 try:
     from pyquickhelper.helpgen.default_conf import set_sphinx_variables, get_default_stylesheet
@@ -10,7 +10,9 @@ except ImportError as e:
         os.environ.get("PYTHONPATH", None), paths))
 
 set_sphinx_variables(__file__, "project_name", "author(s)", 2017,
-                     "wild", wild_sphinx_theme.get_theme_dir(), locals(), book=True,
+                     "sphinx_rtd_theme", [
+                         sphinx_rtd_theme.get_html_theme_path()],
+                     locals(), book=True,
                      extlinks=dict(issue=('https://github.com/sdpython/python3_module_template/issues/%s', 'issue')))
 
 blog_root = "http://www.xavierdupre.fr/app/python3_module_template/helpsphinx/"
