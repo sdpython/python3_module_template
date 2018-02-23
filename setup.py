63278b32441e0c628f9565d65370cc38477e2e51
xavier dupré <xavier.dupre@gmail.com>
2018-02-24 00:29:45 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -49,22 +49,8 @@ def is_local():
     file = os.path.abspath(__file__).replace("\\", "/").lower()
     if "/temp/" in file and "pip-" in file:
         return False
-    for cname in {"bdist_msi", "build27", "build_script", "build_sphinx", "build_ext",
-                  "bdist_wheel", "bdist_egg", "bdist_wininst", "clean_pyd", "clean_space",
-                  "copy27", "copy_dist", "local_pypi", "notebook", "publish", "publish_doc",
-                  "register", "unittests", "unittests_LONG", "unittests_SKIP", "unittests_GUI",
-                  "run27", "sdist", "setupdep", "test_local_pypi", "upload_docs", "setup_hook",
-                  "copy_sphinx", "write_version"}:
-        if cname in sys.argv:
-            try:
-                import_pyquickhelper()
-            except ImportError:
-                return False
-            return True
-    else:
-        return False
-
-    return False
+    from pyquickhelper.pycode.setup_helper import available_commands_list
+    return available_commands_list(sys.argv)
 
 
 def ask_help():
