4c15d5d4d6344c42e9067b4dc77d0e32dd1d49f8
xavier dupré <xavier.dupre@gmail.com>
2018-03-24 22:16:03 +0100

    update script bat

--- build_script.bat
+++ build_script.bat
@@ -1,11 +1,13 @@
 @echo off
+@echo %PYTHONPATH%
 if "%1"=="" goto default_value_python:
 set pythonexe="%1"
 %pythonexe% setup.py write_version
 goto custom_python:
 
 :default_value_python:
-set pythonexe="c:\Python363_x64\python.exe"
+set pythonexe="c:\Python364_x64\python.exe"
+if not exist %pythonexe% set pythonexe="c:\Python363_x64\python.exe"
 if not exist %pythonexe% set pythonexe="c:\Python36_x64\python.exe"
 :custom_python:
 @echo [python] %pythonexe%
