1ad3f1150d7a03ffbee279fb2dcc84f0e5d93694
xavier dupré <xavier.dupre@gmail.com>
2018-05-11 01:46:39 +0200

    rename neg_filter into neg_pattern

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -33,7 +33,7 @@ class TestCodeStyle(ExtTestCase):
     def test_style_test(self):
         thi = os.path.abspath(os.path.dirname(__file__))
         test = os.path.normpath(os.path.join(thi, "..", ))
-        check_pep8(test, fLOG=fLOG, neg_filter="temp_.*",
+        check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*",
                    pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
                                   'C0111'),
                    skip=["src' imported but unused",
