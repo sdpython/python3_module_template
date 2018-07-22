916296e76534adbda02c34f0b655944d9d1dbba2
xavier dupré <xavier.dupre@gmail.com>
2018-07-22 17:32:46 +0200

    update CI

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ if is_local():
         coverage_options=dict(omit=["*exclude*.py"]),
         github_owner=project_owner,
         fLOG=logging_function, covtoken=(
-            "ce1fc48d-9081-44b3-ae09-8b4665e28e86", "'_UT_36_std' in outfile"),
+            "ce1fc48d-9081-44b3-ae09-8b4665e28e86", "'_UT_37_std' in outfile"),
         layout=["rst", "pdf", "epub", "html",
                 ("html", "build2", {"html_theme": "alabaster"}, "source/phdoc_static2")])
     if not r and not ({"bdist_msi", "sdist",
