8a1ff682ac0d33ea645de48f7dd2396d1219e3d8
xavier dupré <xavier.dupre@gmail.com>
2019-06-10 22:13:16 +0200

    coverage

--- _unittests/ut_module/test_setup.py
+++ _unittests/ut_module/test_setup.py
@@ -2,6 +2,8 @@
 @brief      test tree node (time=50s)
 """
 import unittest
+from contextlib import redirect_stdout
+from io import StringIO
 from pyquickhelper.pycode import ExtTestCase
 from python3_module_template import check, _setup_hook
 
@@ -13,6 +15,10 @@ class TestSetup(ExtTestCase):
 
     def test_setup_hook(self):
         _setup_hook()
+
+    def test_setup_hook_print(self):
+        with redirect_stdout(StringIO()):
+            _setup_hook(True)
 
 
 if __name__ == "__main__":
